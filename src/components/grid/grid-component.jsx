4c807265714e518e962eea057f2ee3749e73720a
Syed Thowfeeq Ahmed <Syed.ThowfeeqAhmed@ltts.com>
2020-09-12 22:13:50 +0530

    bug fixes

--- src/components/grid/grid-component.jsx
+++ src/components/grid/grid-component.jsx
@@ -16,7 +16,6 @@ class gridComponent extends React.Component {
 
   handleScroll = () => {
     const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
-    console.log("window.height is -->",windowHeight)
     const body = document.body;
     const html = document.documentElement;
     const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
@@ -29,7 +28,6 @@ class gridComponent extends React.Component {
     }, () => {
       this.appendNextPage()
     });
-        console.log("end is reached")
     } else {
         this.setState({
             isBottomReached: false
@@ -40,7 +38,6 @@ class gridComponent extends React.Component {
    appendNextPage() {
      console.log("debugg this.state.current_page",this.state.current_page)
     if(this.state.current_page > 3) {
-      console.log("all api's are rendered")
       return
     } else {
       console.log("debugg added Data for ",this.state.current_page)
@@ -48,11 +45,9 @@ class gridComponent extends React.Component {
       if(this.state.current_page ===2){
         const dataPage2 =  require('../../api/CONTENTLISTINGPAGE-PAGE2.json');
         initialDataPage2 = dataPage2.page["content-items"].content;
-        console.log("debugg1",initialDataPage2)
       } else {
         const dataPage3 =  require('../../api/CONTENTLISTINGPAGE-PAGE3.json');
         initialDataPage2 = dataPage3.page["content-items"].content
-        console.log("debugg",initialDataPage2)
       }
       this.setState({listContent:  [...this.state.listContent,...initialDataPage2]})
     }
@@ -64,7 +59,6 @@ class gridComponent extends React.Component {
     let initialData = dataPage1.page["content-items"].content
     
     this.setState({listContent:initialData})
-    console.log("data" ,initialData )
 }
 
   componentWillUnmount() {
--- src/components/list/list-component.jsx
+++ src/components/list/list-component.jsx
@@ -5,7 +5,7 @@ class listComponent extends Component {
 
   render() {
     return (
-      <div className="flex flex-wrap -mx-2" style={{height:'300px'}}>
+      <div className="flex flex-wrap -mx-2" style={{height:'100%'}}>
       {this.genarateList()}
       </div>
     );
@@ -13,7 +13,6 @@ class listComponent extends Component {
 
   genarateList=() => {
     return this.props.list.map((content,index) => {
-      console.log(content)
       return (
       <div className=" listItem w-1/3 sm:w-1/3  lg:w-1/4 xl:w-1/6 mb-4 pb-30 px-2" key={index}>
         <ImageComponent image={content}/>
